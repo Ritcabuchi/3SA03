db7dbba5a9adfc4bf5484b718a70a1b014e7c748
ritcabuchi <ridwanritcabuchi@gmail.com>
2018-09-13 10:55:26 +0700

    state

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard';
-export default class WordCard extends
-Component {
-render() {
- return (
- <div>
-{ Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
- }
+export default class WordCard extends Component {
+    constructor(props){
+    super(props)
+    this.state = {
+    active: false,
+    }
+    }
+   
+    activate = () => {
+    if(!this.state.active){
+    this.setState({active: true})
+    }
+    }
+    render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
+    return (
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
+    </div>
+    )
 }
+}  
