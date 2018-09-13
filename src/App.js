598b374feb740bedf9fa1a044df9156bf6788b5a
ritcabuchi <ridwanritcabuchi@gmail.com>
2018-09-13 10:14:54 +0700

    add Charator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CharacterCard from './CharacterCard';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello world
+        hello world 5+5
       </div>
     );
   }
