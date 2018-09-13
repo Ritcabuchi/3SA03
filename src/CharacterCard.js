2727fca995f6c72db8f7fbbca181c933a291e5e6
ritcabuchi <ridwanritcabuchi@gmail.com>
2018-09-13 10:43:51 +0700

    Add Style

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class CharacterCard extends Component {
     render(){
     return (
-    <div>{this.props.value}</div>
+    <div className="App">{this.props.value}</div>
     )
     }
    }
